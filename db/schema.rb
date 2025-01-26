dfcd554d500d55a663c11dce7bfa5080ea1db243
masaki-tec <masaki11221851@gmail.com>
2025-01-26 11:40:13 +0900

    itemテーブルのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_25_202053) do
     t.integer "prefecture_id", null: false
     t.integer "delivery_id", null: false
     t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -72,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_25_202053) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end

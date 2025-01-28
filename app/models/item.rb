e07cbac097bbd0b0f0f336114846b4dc88460762
masaki-tec <masaki11221851@gmail.com>
2025-01-28 21:27:17 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :product_description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :cover_delivery_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  with_options presence: true do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      format: { with: /\A\d+\z/ }
+  end
+
+  belongs_to :user
 end

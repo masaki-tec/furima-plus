bf5a30078ee49544ebf2a549970f4f79ec848399
masaki-tec <masaki11221851@gmail.com>
2025-01-29 03:03:00 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,10 +43,10 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
-      it 'priceがだと登録できない' do
-        @item.price = 299
+      it 'priceが全角だと登録できない' do
+        @item.price = '５００'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
 
       it 'imageがないと登録できない' do
